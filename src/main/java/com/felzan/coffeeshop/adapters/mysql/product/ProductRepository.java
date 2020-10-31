0904dd78ee5e47eb9481541ec0675873139523a9
Luis Felipe Zanatto <luisfelipe.zanatto@gmail.com>
2020-10-30 21:08:14 -0300

    Testing Adding missing @Repository

--- src/main/java/com/felzan/coffeeshop/adapters/mysql/product/ProductRepository.java
+++ src/main/java/com/felzan/coffeeshop/adapters/mysql/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.felzan.coffeeshop.adapters.mysql.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
